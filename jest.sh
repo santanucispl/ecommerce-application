66722f3be2edf47b255f0f8a0ef49676db8ec70e
santanucispl <63183875+santanucispl@users.noreply.github.com>
2024-05-28 16:30:38 +0530

    Update jest.sh

--- jest.sh
+++ jest.sh
@@ -8,23 +8,23 @@ npm run test
 cd ..
 
 
-# cd products
-# npm install
-# npm run test
-# cd ..
+cd products
+npm install
+npm run test
+cd ..
 
 
-# cd orders
-# npm install
-# npm run test
-# cd ..
+cd orders
+npm install
+npm run test
+cd ..
 
-# cd expiration
-# npm install
-# npm run test
-# cd ..
+cd expiration
+npm install
+npm run test
+cd ..
 
-# cd payments
-# npm install
-# npm run test
-# cd ..
+cd payments
+npm install
+npm run test
+cd ..
